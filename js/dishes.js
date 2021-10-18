47e1f022964b9fe9437a8e670b21754a9c66d910
Law <72727117+Law-js@users.noreply.github.com>
2021-10-18 14:52:20 +0200

    Add files via upload

--- js/dishes.js
+++ js/dishes.js
@@ -2,12 +2,15 @@ const getData = async () => {
   const btns = document.querySelectorAll(".overlay-menu button");
   const title = document.querySelector(".main-header h1");
   const cards = document.querySelector(".cards");
+  const cardsFooter = document.querySelector(".cards-footer");
+  const body = document.querySelector("body");
 
   await fetch("./../data.json")
     .then((res) => res.json())
     .then(({ data }) => {
       btns.forEach((btn) => {
         btn.addEventListener("click", () => {
+          body.style.overflowY = "initial";
           let value = btn.value;
           title.textContent = "Nos " + value;
           cards.innerHTML = data
@@ -30,6 +33,13 @@ const getData = async () => {
           `
             )
             .join("");
+          cardsFooter.innerHTML = `
+            <a href="#top" title="Retour en haut de la page">
+            <div class="arrow arrow-first"></div>
+            <div class="arrow arrow-second"></div>
+            <span>Retour haut de page</span>
+          </a>
+            `;
         });
       });
     });
